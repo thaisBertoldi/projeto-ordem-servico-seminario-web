a4fc2a5b6df3e6e6e56ef822a1377f0a09d20b43
micasouza691 <miqueiassouza691@gmail.com>
2023-04-10 20:27:29 -0300

    iniciando componentes da pagina home

--- frontend/service-order-web/src/app/views/components/home/home.component.ts
+++ frontend/service-order-web/src/app/views/components/home/home.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  max = 100;
+  min = 0;
+  step = 1;
+  value = 0;
   constructor() { }
 
   ngOnInit(): void {
