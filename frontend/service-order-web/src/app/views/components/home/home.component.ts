4b4b7d2648b7e4627ce40147beac829c59c97e2a
micasouza691 <miqueiassouza691@gmail.com>
2023-04-13 14:06:17 -0300

    continuacao home

--- frontend/service-order-web/src/app/views/components/home/home.component.ts
+++ frontend/service-order-web/src/app/views/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OS, Person } from 'src/app/models/Interfaces';
+import { CustomerService } from 'src/app/services/customer.service';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { ServiceOrderService } from 'src/app/services/serviceorder.service';
 
 @Component({
   selector: 'app-home',
@@ -6,13 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  max = 100;
-  min = 0;
-  step = 1;
-  value = 0;
-  constructor() { }
+  numberEmployees: Number;
+  employees: Number;
+  openOS: number = 0;
+  closedOS: number = 0;
+  customers: Number;
 
-  ngOnInit(): void {
+  constructor(
+    private serviceOrder: ServiceOrderService,
+    private employeeService: EmployeeService,
+    private customerService: CustomerService
+  ) { 
+    this.getOrders();
+    this.getAllCustomers();
+    this.getAllEmployees();
   }
 
+  ngOnInit(): void {
+
+  }
+
+  getOrders() {
+    this.serviceOrder.findAll().subscribe(resp => {
+      resp.forEach(order => {
+        if (order.status === "ANDAMENTO" || order.status === "ABERTO") {
+          this.openOS += 1;
+        } else {
+          this.closedOS += 1;
+        }
+      })
+    });
+  }
+
+  getAllEmployees() {
+    this.employeeService.findAll().subscribe(resp => {
+      this.employees = resp.length;
+    });
+  }
+
+  getAllCustomers() {
+    this.customerService.findAll().subscribe(resp => {
+      this.customers = resp.length;
+    });
+  }
 }
